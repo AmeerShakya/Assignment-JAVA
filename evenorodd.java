87b96675cc5ca9383ad0fa941245d014f06c7fc4
AmeerShakya <160707144+AmeerShakya@users.noreply.github.com>
2024-03-08 13:55:30 +0545

    evenorodd.java

--- evenorodd.java
+++ evenorodd.java
@@ -1,20 +1,15 @@
 import java.util.Scanner;
-
 public class evenorodd {
-    
     public static void main(String[] args) {
         Scanner out= new Scanner(System.in);
-
         System.out.println("Enter a number:");
         int n = out.nextInt();
-
         if (n%2 == 0) {
             System.out.println(n+"It is even");
         } 
         else {
             System.out.println(n+"It is odd");
         }
-
         out.close();
     }
 }
